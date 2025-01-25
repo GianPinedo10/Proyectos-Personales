4592b80d0018e08028e6786f8c79ec7bf9b1fb68
GianPinedo10 <gianpinedo10@gmail.com>
2025-01-25 15:57:20 -0500

    Update Consultas SQL.sql

--- Proyecto2- Análisis de Ventas de Pizza/Consultas SQL.sql
+++ Proyecto2- Análisis de Ventas de Pizza/Consultas SQL.sql
@@ -1,3 +1,14 @@
 -- 1. Total de ingresos por las órdenes de pizza 
-SELECT SUM(total_price) AS IngresosTotales from pizza_sales
+SELECT SUM(total_price) AS Total_Ingresos from pizza_sales
+-- 1.1 Total de ingresos por categoría 
+SELECT 
+    pizza_category, 
+    CAST(SUM(total_price) AS DECIMAL(10, 2)) AS Total_Ingresos
+FROM 
+    pizza_sales
+GROUP BY 
+    pizza_category
+ORDER BY 
+    Total_Ingresos DESC;
+
 
