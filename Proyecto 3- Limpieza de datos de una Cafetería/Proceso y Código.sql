f9f9e165ca0dd15b91780703c962bf4a6966c3c9
GianPinedo10 <gianpinedo10@gmail.com>
2025-01-28 21:23:00 -0500

    Update Proceso y Código.sql

--- Proyecto 3- Limpieza de datos de una Cafetería/Proceso y Código.sql
+++ Proyecto 3- Limpieza de datos de una Cafetería/Proceso y Código.sql
@@ -43,4 +43,50 @@ SELECT
 FROM 
     cafe_sales;
 -- 3. Para rellenar las celdas que contienen valores nulos se realiza lo siguiente en cada columna: 
--- 
+--  Price_Per_Unit:
+-- Se usa un Self Join
+UPDATE a
+SET a.Price_Per_Unit = b.Price_Per_Unit
+FROM cafe_sales a
+JOIN cafe_sales b
+    ON a.Item = b.Item
+WHERE a.Price_Per_Unit IS NULL 
+    AND b.Price_Per_Unit IS NOT NULL;
+-- Los valores nulos que quedan se terminan rellenando dividiendo el Total_Spent entre Quantity
+UPDATE cafe_sales
+SET Price_Per_Unit = Total_Spent / Quantity
+WHERE Price_Per_Unit IS NULL AND Quantity > 0;
+-- Quantity:
+-- Se rellenan los valores nulos diviendo el Total_Spent entre Price_Per_Unit
+UPDATE cafe_sales
+SET Quantity = Total_Spent / Price_Per_Unit
+WHERE Quantity IS NULL AND Price_Per_Unit > 0;
+-- Los valores nulos que quedan se terminan rellenando con valores aleatorios entre 1 y 5 ya que no había otra manera de saber la cantidad de productos que compra cada cliente
+UPDATE cafe_sales
+SET Quantity = FLOOR(RAND(CHECKSUM(NEWID())) * 5) + 1
+WHERE Quantity IS NULL;	
+-- Total_Spent: 
+--
+UPDATE cafe_sales
+SET Total_Spent = Quantity * Price_Per_Unit
+WHERE Total_Spent IS NULL
+-- Después es necesario eliminar 6 filas restantes para que el precio tenga consistencia con los demás datos 
+DELETE FROM cafe_sales
+WHERE Price_Per_Unit IS NULL;
+Al hacer esto se tiene por ahora en valores Nulos de quantity 0, valores nulos en Price_per_unit 0 y en total_spent 0 
+-- Payment_method:
+-- Aquí se usa una subconsulta y se rellena los valores nulos con el valor más común(moda)
+UPDATE cafe_sales
+SET payment_method = (
+    SELECT TOP 1 Payment_method
+    FROM cafe_sales
+    WHERE Payment_method IS NOT NULL
+    GROUP BY Payment_method
+    ORDER BY COUNT(*) DESC
+
+WHERE payment_method IS NULL;
+-- Location:
+
+
+
+
